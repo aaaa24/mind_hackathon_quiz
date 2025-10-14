4e9aa4ccef8cd47cbbe4621572f266a1a287ecb3
aaaa24 <90201411+aaaa24@users.noreply.github.com>
2025-10-14 14:08:11 +0300

    Выполнена подготовка для развёртывания на сервере

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from app import create_app
-from app.sockets import init_socketio
+from app import create_app, socketio
 
 app = create_app()
-init_socketio(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Для разработки
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
