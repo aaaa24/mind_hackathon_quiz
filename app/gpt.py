a8dbe3e27ba5151bb914368594026a3d36080604
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-15 11:41:48 +0300

    Немного настроен gpt

--- app/gpt.py
+++ app/gpt.py
@@ -24,7 +24,7 @@ def gpt_request(system, text):
     response = ai_client.chat.completions.create(
         model='gemini-2.5-flash-lite',
         messages=messages,
-        temperature=0.8
+        temperature=1
     )
     text = response.choices[0].message.content
 
@@ -33,11 +33,11 @@ def gpt_request(system, text):
 
 def get_gpt_questions(count_questions, category_ids):
     if len(category_ids) == 1:
-        system = "Необходимо придумать n вопросов на тематики: topics по запросу пользователей. Вернуть нужно только список словарей в формате [{'text': (Текст вопроса),'options': {[(ответ_1), (ответ_2), (ответ_3), (ответ_4)]}, 'correct_answer': (правильный ответ), 'time_limit': (Ограничение времени на вопрос от 30 до 60)}]."
-        user_text = f"n = {count_questions}, topics = Любая тематика на твое усмотрение"
+        system = "Необходимо придумать n вопросов на тематики: topics по запросу пользователей. Вернуть нужно только список словарей в формате [{'text': (Текст вопроса),'options': {[(ответ_1), (ответ_2), (ответ_3), (ответ_4)]}, 'correct_answer': (правильный ответ), 'time_limit': (Ограничение времени на вопрос от 30 до 60)}]. Делай интересные и уникальные вопросы. Вопросы не должны быть банальными"
+        user_text = f"n = {count_questions}, topics = Любая тематика на твое усмотрение. Делай интересные и уникальные вопросы"
     else:
         category_ids.remove(os.getenv('GPT_CATEGORY_ID'))
-        system = "Необходимо придумать n вопросов на тематики: topics по запросу пользователей. Вернуть нужно только список словарей в формате [{'text': (Текст вопроса),'options': {[(ответ_1), (ответ_2), (ответ_3), (ответ_4)]}, 'correct_answer': (правильный ответ), 'time_limit': (Ограничение времени на вопрос от 30 до 60)}]."
+        system = "Необходимо придумать n вопросов на тематики: topics по запросу пользователей. Вернуть нужно только список словарей в формате [{'text': (Текст вопроса),'options': {[(ответ_1), (ответ_2), (ответ_3), (ответ_4)]}, 'correct_answer': (правильный ответ), 'time_limit': (Ограничение времени на вопрос от 30 до 60)}]. Делай интересные и уникальные вопросы. Вопросы не должны быть банальными"
         user_text = f"n = {count_questions}, topics = {category_ids}"
     try:
         data = gpt_request(system, user_text)
